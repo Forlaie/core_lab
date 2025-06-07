259f51d8a6462247a5967288cd69a45b75e7c50a
wenjess1 <jessica.wen@mail.utoronto.ca>
2025-06-07 14:43:20 -0400

    updated about msg

--- Components/About/About.jsx
+++ Components/About/About.jsx
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <section className='px-30 py-10 mx-auto'>
         <h1 className='text-2xl font-semibold text-[#0b3a72] pb-2 border-b border-b-[#f1f2f3]'>Welcome to CORE Lab!</h1>
-        <p className='pt-2'>This is a test to see how this works</p>
+        <p className='pt-2'>
+            This is a lab for the cool kids &gt;:) <br/>
+            Also this is totally ripping off the style of the robotics lab website rn, I am still figuring out how this stuff works
+        </p>
     </section>
   )
 }
