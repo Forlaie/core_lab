37f1173f32488f93add354c97d50c19c7668fb2b
wenjess1 <jessica.wen@mail.utoronto.ca>
2025-06-08 22:31:59 -0400

    add years, add search bar

--- data/bios.js
+++ data/bios.js
@@ -2,16 +2,22 @@ import genericImg from '../Assets/generic.png';
 
 const members = [
   {
+    name: "Michael Liut",
+    image: genericImg,
+    bio: "Michael is a very cool and chill prof, he is down for the shenanigans",
+    website: "https://www.michaelliut.ca/"
+  },
+  {
+    name: "Andrew Petersen",
+    image: genericImg,
+    bio: "Shoutout to Andrew for letting me take over his office artistically",
+    website: "https://utmandrew.bitbucket.io/"
+  },
+  {
     name: "Naaz Sibia",
     image: genericImg,
     bio: "Naaz is a PHD student and she is very cool :D",
     website: "https://www.naazsibia.com/"
-  },
-  {
-    name: "Michael Liut",
-    image: genericImg,
-    bio: "Michael Liut is a very cool and chill prof, he is down for the shenanigans",
-    website: "https://www.michaelliut.ca/"
   }
 ];
 
