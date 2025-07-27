76e01b4fa24099d9a70eef69b551cef95e5cee35
wenjess1 <jessica.wen@mail.utoronto.ca>
2025-07-27 14:00:48 -0400

    cool new front page

--- Components/Artwork/Artwork.jsx
+++ Components/Artwork/Artwork.jsx
@@ -1,116 +1,24 @@
 "use client"
 
-import React, { useRef } from 'react'
+import React from 'react'
 import Image from 'next/image';
-import './Artwork.css'
-import next_icon from '../../Assets/next_icon.png'
-import back_icon from '../../Assets/back_icon.png'
-import ml_name from '../../Assets/ml_name.png'
-import ap_name from '../../Assets/ap_name.png'
-import door from '../../Assets/door.png'
-import window from '../../Assets/window.png'
-import whiteboard from '../../Assets/whiteboard.png'
+import { art } from '@/data/artwork';
 
 const Artwork = () => {
-
-    const slider = useRef();
-    let tx = 0;
-
-    const slideForward = ()=>{
-        if (tx > -50){
-            tx -= 20;
-        }
-        slider.current.style.transform = `translateX(${tx}%)`;
-    }
-
-    const slideBackward = ()=>{
-        if (tx < 0){
-            tx += 20;
-        }
-        slider.current.style.transform = `translateX(${tx}%)`
-    }
 
   return (
     <div className='px-30 py-10 mx-auto relative'>
       <h1 className='text-2xl font-semibold text-[#0b3a72] pb-2 border-b border-b-[#f1f2f3]'>Our Artwork :D</h1>
-      <p className='pt-2'>Not only do we do research, but we also know how to have fun! Here are some of the artworks made by some of the members in our lab.</p>
-      <Image src={next_icon} alt="" className='next-btn' onClick={slideForward}/>
-      <Image src={back_icon} alt="" className='back-btn' onClick={slideBackward}/>
-      <div className="slider">
-        <ul ref={slider}>
-            <li>
-                <div className="slide">
-                    <div className="art-info">
-                        <Image src={ml_name} alt="" />
-                        <div>
-                            <h3>Artist: Jess</h3>
-                        </div>
-                    </div>
-                    <p>
-                        Prof Michael's namecard decorated!<br/>
-                        Shoutout to Michael for introducting me to research, it's been a blast ever since &gt;:)
-                    </p>
-                </div>
-            </li>
-            <li>
-                <div className="slide">
-                    <div className="art-info">
-                        <Image src={ap_name} alt="" />
-                        <div>
-                            <h3>Artist: Jess</h3>
-                        </div>
-                    </div>
-                    <p>
-                        Prof Andrew's namecard decorated!<br/>
-                        If you ever walk across his door or enter his office, you'll see the absolute masterpiece (mess) I've made there. Thanks for letting your office be my canvas, Andrew :P
-                    </p>
-                </div>
-            </li>
-            <li>
-                <div className="slide">
-                    <div className="art-info">
-                        <Image src={whiteboard} alt="" />
-                        <div>
-                            <h3>Artist: Jess</h3>
-                        </div>
-                    </div>
-                    <p>
-                        My magnum opus.<br/>
-                        This is the culmination of ~2 years worth of office hours, starting from the second half of first year winter semester, all the way through second year.<br/>
-                        The goal is to keep adding on to this whiteboard throughout my university life :D
-                    </p>
-                </div>
-            </li>
-            <li>
-                <div className="slide">
-                    <div className="art-info">
-                        <Image src={door} alt="" />
-                        <div>
-                            <h3>Artist: Jess</h3>
-                        </div>
-                    </div>
-                    <p>
-                        End of year party at Andrew's office!!!<br/>
-                        Do NOT steal the Pokemon cards, we're all watching you...
-                    </p>
-                </div>
-            </li>
-            <li>
-                <div className="slide">
-                    <div className="art-info">
-                        <Image src={window} alt="" />
-                        <div>
-                            <h3>Artist: Jess</h3>
-                        </div>
-                    </div>
-                    <p>
-                        Tetris!!!<br/>
-                        I <i>was</i> gonna make a computer and cut out alphabet letters so that you could write messages on the window, but then saw this Tetris thing and thought it would be more fun instead<br/>
-                        (Also easier to make, because I don't need to cut out a bunch of alphabet letters)
-                    </p>
-                </div>
-            </li>
-        </ul>
+      <p className='pt-2'>Not only do we do research, but we also know how to have fun! Here are some of the artworks made by some of the members in our lab:</p>
+      
+      <div className='grid grid-cols-auto my-5 gap-6'>
+        {art.map((artwork, index)=>(
+            <div className='border border-gray-400 rounded-lg px-5 py-5 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500' key={index}>
+                <Image src={artwork.image} alt="" className=''/>
+                <h3 className='text-lg my-4 text-[#0b3a72] font-semibold'>Artist: {artwork.artist}</h3>
+                <p className='text-sm text-gray-600 leading-5'>{artwork.description}</p>
+            </div>
+        ))}
       </div>
     </div>
   )
---
+++ data/artwork.js
@@ -0,0 +1,48 @@
+import ml_name from '../Assets/ml_name.png'
+import ap_name from '../Assets/ap_name.png'
+import lz_name from '../Assets/lz_name.png'
+import whiteboard from '../Assets/whiteboard.png'
+import door from '../Assets/door.png'
+import window from '../Assets/window.png'
+
+export const assets = {
+    ml_name,
+    ap_name,
+    lz_name,
+    whiteboard,
+    door,
+    window
+};
+
+export const art = [
+    {
+        artist: "Jess",
+        description: "Prof Michael's namecard decorated! Shoutout to Michael for introducting me to research, it's been a blast ever since >:)",
+        image: assets.ml_name,
+    },
+    {
+        artist: "Jess",
+        description: "Prof Andrew's namecard decorated! If you ever walk across his door or enter his office, you'll see the absolute masterpiece (mess) I've made there.",
+        image: assets.ap_name,
+    },
+    {
+        artist: "Jess",
+        description: "Prof Andrew's namecard decorated! If you ever walk across his door or enter his office, you'll see the absolute masterpiece (mess) I've made there. Thanks for letting your office be my canvas, Andrew :P",
+        image: assets.lz_name,
+    },
+    {
+        artist: "Jess",
+        description: "My magnum opus. This is the culmination of ~2 years worth of office hours, starting from the second half of first year winter semester, all the way through second year. The goal is to keep adding on to this whiteboard throughout my university life :D",
+        image: assets.whiteboard,
+    },
+    {
+        artist: "Jess",
+        description: "End of year party at Andrew's office!!! Do NOT steal the Pokemon cards, we're all watching you...",
+        image: assets.door,
+    },
+    {
+        artist: "Jess",
+        description: "Tetris!!! I was gonna make a computer and cut out alphabet letters so that you could write messages on the window, but then saw this Tetris thing and thought it would be more fun instead (Also easier to make, because I don't need to cut out a bunch of alphabet letters)",
+        image: assets.window,
+    }
+]
