7d1608600604ed926444ed4eb9028b41afc3c9cb
wenjess1 <jessica.wen@mail.utoronto.ca>
2025-07-28 10:23:45 -0400

    changed project formatting

--- Components/All_Projects/Active.jsx
+++ Components/All_Projects/Active.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Projects.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import voiceex from '../../Assets/voiceex.png'
@@ -11,11 +10,11 @@ const Active = () => {
         <p className='pt-2'>
             These are the active projects we have going on in our lab!
         </p>
-        <div className="project">
-            <Image src={voiceex} alt=""></Image>
-            <div className="project-info">
-                <Link href="/projects/voiceex"><h4>VoiceEx</h4></Link>
-                <p>
+        <div className="flex flex-col lg:flex-row pt-5">
+            <Image src={voiceex} alt="" className='pb-5 lg:pr-4 w-full lg:w-[20%]'></Image>
+            <div className="block">
+                <Link href="/projects/voiceex"><h4 className='m-0 text-[#002a5c] text-3xl font-semibold cursor-pointer inline hover:underline'>VoiceEx</h4></Link>
+                <p className='m-0 text-[#002a5c]'>
                     VoiceEx is a voice explanations system we've been developing over the past couple years.
                     Primarily focused on self-explanations to enhance student learning, we've gone from prompting text self-explanations to voice self-explanations.
                     Using VoiceEx, instructors can prompt students to reflect on a specific question/topic, which students then record a self-explanation to.
--- Components/All_Projects/Past.jsx
+++ Components/All_Projects/Past.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Projects.css'
 import Image from 'next/image'
 
 const Past = () => {
