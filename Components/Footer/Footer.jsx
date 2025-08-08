098539ddfc6093500aa46d99a16b92abe6bec8e2
wenjess1 <jessica.wen@mail.utoronto.ca>
2025-08-08 00:17:45 -0400

    temp footer/contact us

---
+++ Components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+import email_icon from '../../Assets/email_icon.png'
+
+const Footer = () => {
+  return (
+    <div className='px-30 pt-10 mx-auto'>
+      <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mt-12 py-6'>
+        <p>Want to contact us or learn more?</p>
+        <ul className='flex flex-col items-front gap-2 justify-center mt-4 sm:mt-0'>
+            <li className='flex gap-1'> <Image src={email_icon} className="w-7"/> <a href="mailto:learningcs.utm@utoronto.ca" className="text-blue-600 hover:underline">learningcs.utm@utoronto.ca</a></li>
+            <li className='flex gap-1'> <Image src={email_icon} className="w-7"/> <a href="mailto:michael.liut@utoronto.ca" className="text-blue-600 hover:underline">michael.liut@utoronto.ca</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/Navbar/Navbar";
+import Footer from "@/Components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
